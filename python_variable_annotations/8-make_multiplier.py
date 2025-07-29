56af7ed47a2c033afd2e579e8a1df86d2c744916
adashp02 <adashp02@gmail.com>
2025-07-29 17:35:46 +1000

    returns function that multiplies floats

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""type-annotated function that takes a float multiplier
+as argument and returns a function that multiplies a float
+by multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """returns a function that multiplies float by multiplier"""
+    def multiply(n: float) -> float:
+        """returns a float multiplied by multiplier"""
+        return n * multiplier
+    return multiply
