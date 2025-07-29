229a5da8ac1c98436dc81db3c97bccf3e4e0cadc
adashp02 <adashp02@gmail.com>
2025-07-29 16:25:00 +1000

    returns the string representation of the float n

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""type-annotated function to_str that takes a float n
+as argument and returns the string representation of
+the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """and returns the string representation of the float n"""
+    return str(n)
